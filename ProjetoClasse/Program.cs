aa65d1bc688c5a9b541f3c2e394e81f6da26867d
Ranier Dalton do Couto <90422755+RanierDC@users.noreply.github.com>
2022-08-03 11:21:10 -0300

    Add files via upload

--- ProjetoClasse/Program.cs
+++ ProjetoClasse/Program.cs
@@ -10,12 +10,22 @@ namespace ProjetoClasse
     {
         static void Main(string[] args)
         {
-            Pessoa p = new Pessoa();
-            Console.WriteLine(p.ToString());
+            Console.WriteLine("Digite o nome da pessoa: ");
+            string n = Console.ReadLine();
+            Console.WriteLine("Digite a idade de {0}: ", n);
+            int i = int.Parse(Console.ReadLine());
 
-            p.Nome = "Tiago";
+            Console.WriteLine("Digite a altura de {0}: ", n);
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a letra equivalente ao sexo (F/M): ");
+            string s = Console.ReadLine();
 
-            Console.WriteLine(p);
+            Pessoa pessoa = new Pessoa(n, i, a, s);
+            Console.WriteLine("\n\n *** Dados da Pessoa *** \n\n");
+            Console.WriteLine(pessoa.ToString());
+
+            Console.WriteLine("Peso Ideal: {0}", pessoa.calcularPesoIdeal());
+
             Console.ReadKey();
         }
     }
