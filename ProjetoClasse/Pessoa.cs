a3df1f0aebf42e07e88dedfc9bdb97bf58202ea8
Ranier Dalton do Couto <90422755+RanierDC@users.noreply.github.com>
2022-08-08 10:40:48 -0300

    Add files via upload

--- ProjetoClasse/Pessoa.cs
+++ ProjetoClasse/Pessoa.cs
@@ -13,6 +13,8 @@ namespace ProjetoClasse
         private int idade;
         private double altura;
         private string sexo;
+        private double massa;
+        private string imc;
 
         public Pessoa()
         {
@@ -20,14 +22,18 @@ namespace ProjetoClasse
             this.idade = 0;
             this.altura = 0.0;
             Sexo = null;
+            this.massa = 0.0;
+            this.imc = null;
         }
 
-        public Pessoa (string nome, int idade, double altura, string sexo)
+        public Pessoa (string nome, int idade, double altura, string sexo, double massa, string imc)
         {
             this.nome = nome;
             this.idade = idade;
             this.altura = altura;
             Sexo = sexo.ToString();
+            this.massa = massa;
+            this.imc = imc;
         }
 
         public string Nome
@@ -54,6 +60,18 @@ namespace ProjetoClasse
             set { sexo = value.ToUpper() == "M" ? "MASCULINO" : "FEMININO"; }   
         }
 
+        public double Massa
+        {
+            get { return massa; }
+            set { massa = value; }
+        }
+
+        public string Imc
+        {
+            get { return this.imc; }
+            set { this.imc = value; }
+        }
+
         public double calcularPesoIdeal()
         {
             if(sexo.Equals("MASCULINO"))
@@ -66,9 +84,53 @@ namespace ProjetoClasse
             }
         }
 
+        public double calcularIMC()
+        {
+            if(sexo.Equals("MASCULINO"))
+            {
+                return massa / (altura * altura);
+            }
+            else
+            {
+                return massa / (altura * altura);
+            }
+        }
+
+        public string mostrarIMC()
+        {
+            float imc;
+            imc = m / (a * a);
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Peso abaixo do normal");
+            }
+            else if ((imc == 18.5) || (imc == 25))
+            {
+                Console.WriteLine("Peso normal");
+            }
+            if ((imc > 25) || (imc == 30))
+
+            {
+                Console.WriteLine("Sobre o Peso");
+            }
+            else if ((imc > 30) || (imc == 35))
+            {
+                Console.WriteLine("Grau de Obesidade I");
+            }
+            if ((imc > 35) || (imc == 40))
+            {
+                Console.WriteLine("Grau de Obesidade II");
+            }
+            else if (imc > 40)
+            {
+                Console.WriteLine("Obesidade Grau III");
+            }
+        }
+
         public override string ToString()
         {
-            return (String.Format("Nome: {0} \n Idade: {1} \n Altura: {2} \n Sexo: {3}\n ", nome, idade, altura, sexo)); 
+            return (String.Format("Nome: {0} \nIdade: {1} \nAltura: {2} \nSexo: {3}\n ", nome, idade, altura, sexo, imc)); 
         }
 
     }
--- ProjetoClasse/Program.cs
+++ ProjetoClasse/Program.cs
@@ -10,21 +10,29 @@ namespace ProjetoClasse
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Digite o nome da pessoa: ");
             string n = Console.ReadLine();
             Console.WriteLine("Digite a idade de {0}: ", n);
             int i = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite a altura de {0}: ", n);
+            Console.WriteLine("Digite a altura em metros de {0}: ", n);
             double a = double.Parse(Console.ReadLine());
             Console.WriteLine("Digite a letra equivalente ao sexo (F/M): ");
             string s = Console.ReadLine();
 
-            Pessoa pessoa = new Pessoa(n, i, a, s);
+           
+
+            //CALCULAR IMC  
+            Console.WriteLine("\n *** Cálculo de IMC *** \n");
+            Console.WriteLine("Digite a massa atual de {0} em KG: ", n);
+            double m = double.Parse(Console.ReadLine());
+
+            Pessoa pessoa = new Pessoa(n, i, a, s, m, c);
             Console.WriteLine("\n\n *** Dados da Pessoa *** \n\n");
             Console.WriteLine(pessoa.ToString());
+            Console.WriteLine("O IMC de {0} é: {1}, que é considerado {2}.", n, pessoa.calcularIMC() );
 
-            Console.WriteLine("Peso Ideal: {0}", pessoa.calcularPesoIdeal());
 
             Console.ReadKey();
         }
